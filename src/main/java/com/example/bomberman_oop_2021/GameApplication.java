9efb2e741bc788feff3e2beaba560e845f5b0f4b
lamcherry0208 <lamcherry0208@gmail.com>
2021-11-18 01:15:32 +0700

    Update Project Structure

---
+++ src/main/java/com/example/bomberman_oop_2021/GameApplication.java
@@ -0,0 +1,16 @@
+package com.example.bomberman_oop_2021;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class GameApplication extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
